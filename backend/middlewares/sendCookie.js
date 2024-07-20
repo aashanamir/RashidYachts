9fb99da5f2bf26d15bec0e7932b535426603164f
aashanamir <emailstormfacts@gmail.com>
2024-07-20 10:44:25 +0500

    Some Changes in Cookie Options

--- backend/middlewares/sendCookie.js
+++ backend/middlewares/sendCookie.js
@@ -1,26 +1,23 @@
 export const sendCookie = (user, res, statusCode, msg) => {
-
   const token = user.getJwtToken();
 
+  const cookieOptions = {
+    httpOnly: true,
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+    sameSite: 'None',
+    secure: true,
+  };
+
   if (msg !== undefined) {
-    res.status(statusCode).cookie("token", token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-    }).json({
+    res.status(statusCode).cookie("token", token, cookieOptions).json({
       success: true,
       user,
       message: msg,
-    })
+    });
   } else {
-    res.status(statusCode).cookie("token", token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-    }).json({
+    res.status(statusCode).cookie("token", token, cookieOptions).json({
       success: true,
       user,
-    })
+    });
   }
-
-
-
 }
