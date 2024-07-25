324e19f9534ece58cb375e7ddd063e632b6bca31
farhan4201659 <farhan.itc86@gmail.com>
2024-07-26 00:08:00 +0500

    Redeploy server

--- frontend/src/API/Baseurl.js
+++ frontend/src/API/Baseurl.js
@@ -1,5 +1,5 @@
-// export const BASEURL = "http://localhost:5000/api/v1/";
+export const BASEURL = "http://localhost:5000/api/v1/";
 
-export const BASEURL = "https://rashid-yachts-backend.vercel.app/api/v1/";
+// export const BASEURL = "https://rashid-yachts-backend.vercel.app/api/v1/";
 
 const app = "Abraham";
